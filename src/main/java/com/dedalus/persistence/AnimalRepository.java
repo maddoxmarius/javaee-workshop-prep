031b1e572a5459d844f0f4f54abca3f898951a75
awpxx <markus.graf@dedalus.com>
2024-06-11 15:09:25 +0200

    Animal repository created

---
+++ src/main/java/com/dedalus/persistence/AnimalRepository.java
@@ -0,0 +1,26 @@
+package com.dedalus.persistence;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.dedalus.model.AnimalEntity;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@ApplicationScoped
+public class AnimalRepository {
+    @Inject
+    EntityManager em;
+
+    public AnimalEntity save(AnimalEntity entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    public List<AnimalEntity> list() {
+        return em.createQuery("SELECT a FROM AnimalEntity a", AnimalEntity.class)
+                .getResultList();
+    }
+}
--- src/main/java/com/dedalus/resources/AnimalResource.java
+++ src/main/java/com/dedalus/resources/AnimalResource.java
@@ -1,24 +1,32 @@
 package com.dedalus.resources;
 
-import com.dedalus.model.AnimalEntity;
-import com.dedalus.persistence.GreetingRepository;
-
+import java.util.List;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.dedalus.model.AnimalEntity;
+import com.dedalus.persistence.AnimalRepository;
+
 @Path("animal")
 public class AnimalResource {
     @Inject
-    GreetingRepository repository;
+    AnimalRepository repository;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    public List<AnimalEntity> list() {
+        return repository.list();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public AnimalEntity list() {
-        return null;
+    public AnimalEntity save(AnimalEntity entity) {
+        return repository.save(entity);
     }
 }
