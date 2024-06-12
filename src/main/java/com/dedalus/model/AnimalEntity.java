49724b037b13fc57b536d45dc2c58d70cac796b5
drame <abdoulaye.drame@dedalus.com>
2024-06-12 13:55:20 +0200

    adds mockito for test and test for animal detailed view.

--- src/main/java/com/dedalus/model/AnimalEntity.java
+++ src/main/java/com/dedalus/model/AnimalEntity.java
@@ -5,7 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
--- src/test/java/com/dedalus/AnimalResourceTest.java
+++ src/test/java/com/dedalus/AnimalResourceTest.java
@@ -2,15 +2,34 @@ package com.dedalus;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+import com.dedalus.model.AnimalEntity;
+import com.dedalus.model.AnimalType;
+import com.dedalus.service.AnimalService;
 
 @QuarkusTest
 public class AnimalResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+
+        AnimalService animalServiceMock = mock(AnimalService.class);
+        AnimalEntity animal = new AnimalEntity();
+        animal.setId(1L);
+        animal.setName("Lion");
+        animal.setType(AnimalType.CAT);
+        animal.setComment("King of the Jungle");
+        animal.setIsAvailable(true);
+        when(animalServiceMock.list()).thenReturn(List.of(animal));
         given()
           .when().get("/hello")
           .then()
