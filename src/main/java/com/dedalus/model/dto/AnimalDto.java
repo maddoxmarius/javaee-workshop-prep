7dfd7b8ecfb78fd14032fdfb6b2bb41514da5ebe
marius.bordeianu <marius.bordeianu@dedalus.eu>
2024-06-13 11:53:23 +0200

    id for AnimalDto

--- src/main/java/com/dedalus/model/dto/AnimalDto.java
+++ src/main/java/com/dedalus/model/dto/AnimalDto.java
@@ -9,23 +9,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class AnimalDto {
-
+    private Long id;
     private String name;
     private AnimalType animalType;
     String comment;
     Boolean isAvailable;
-
-    public AnimalDto(String name, AnimalType animalType)
-    {
-        this.name = name;
-        this.animalType = animalType;
-    }
-
-    public AnimalDto(String name, AnimalType animalType, String comment, Boolean isAvailable)
-    {
-        this.name = name;
-        this.animalType = animalType;
-        this.comment = comment;
-        this.isAvailable = isAvailable;
-    }
 }
