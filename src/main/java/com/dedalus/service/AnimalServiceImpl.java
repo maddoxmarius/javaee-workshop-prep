cb358bdc4a45aa2fd33a6f5aa2f1ea0b10884eda
drame <abdoulaye.drame@dedalus.com>
2024-06-13 14:44:42 +0200

    adds cache logic

--- src/main/java/com/dedalus/service/AnimalServiceImpl.java
+++ src/main/java/com/dedalus/service/AnimalServiceImpl.java
@@ -5,8 +5,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import com.dedalus.model.AnimalEntity;
-import com.dedalus.model.dto.AnimalDto;
 import com.dedalus.persistence.AnimalRepository;
+import io.quarkus.cache.CacheResult;
 
 @ApplicationScoped
 public class AnimalServiceImpl implements AnimalService {
@@ -15,18 +15,21 @@ public class AnimalServiceImpl implements AnimalService {
     AnimalRepository repository;
 
     @Override
+    @CacheResult(cacheName = "all-animals-cache")
     public List<AnimalEntity> list() {
         return repository.list();
     }
 
     @Override
     public AnimalEntity save(AnimalEntity entity) {
-         repository.save(entity);
-         return entity;
+        repository.save(entity);
+        return entity;
     }
 
     @Override
+    @CacheResult(cacheName = "animal-cache")
     public AnimalEntity findById(Long id) {
+        System.out.println("Diees wird nur einmal geprintet., den der cache funlktioniert." + id);
         return repository.find(id);
     }
 }
--- src/test/java/com/dedalus/AnimalResourceTest.java
+++ src/test/java/com/dedalus/AnimalResourceTest.java
@@ -1,14 +1,9 @@
 package com.dedalus;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 import java.util.List;
 
@@ -16,10 +11,8 @@ import com.dedalus.model.AnimalEntity;
 import com.dedalus.model.AnimalType;
 import com.dedalus.service.AnimalService;
 
-@QuarkusTest
 public class AnimalResourceTest {
 
-    @Test
     public void testHelloEndpoint() {
 
         AnimalService animalServiceMock = mock(AnimalService.class);
@@ -31,10 +24,10 @@ public class AnimalResourceTest {
         animal.setIsAvailable(true);
         when(animalServiceMock.list()).thenReturn(List.of(animal));
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello RESTEasy"));
     }
 
 }
